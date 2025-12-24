ea540c9c0593631dcc66583a7bb964b140c2501c
Stefan Hoekzema <57985160+shoekzema@users.noreply.github.com>
2025-12-24 18:54:29 +0100

    color descriptions

--- src/window/TimelineView.h
+++ src/window/TimelineView.h
@@ -16,17 +16,17 @@ struct TimelineView {
     int rowHeight = trackHeight + 2;
 
     // Colors
-    SDL_Color videoTrackBGColor      = { 35,  38,  41, 255 };
-    SDL_Color videoTrackDataColor    = { 33,  36,  39, 255 };
-    SDL_Color videoTrackSegmentColor = { 19, 102, 162, 255 };
+    SDL_Color videoTrackBGColor      = { 35,  38,  41, 255 }; // Desaturated Blueish
+    SDL_Color videoTrackDataColor    = { 33,  36,  39, 255 }; // Darker Desaturated Blueish
+    SDL_Color videoTrackSegmentColor = { 19, 102, 162, 255 }; // Blue
 
-    SDL_Color audioTrackBGColor      = { 40, 37, 45, 255 };
-    SDL_Color audioTrackDataColor    = { 38, 35, 43, 255 };
-    SDL_Color audioTrackSegmentColor = { 13, 58, 32, 255 };
+    SDL_Color audioTrackBGColor      = { 40, 37, 45, 255 }; // Desaturated Purplish
+    SDL_Color audioTrackDataColor    = { 38, 35, 43, 255 }; // Darker Desaturated Purplish
+    SDL_Color audioTrackSegmentColor = { 13, 58, 32, 255 }; // Dark Green
 
-    SDL_Color segmentOutlineColor   = {  61, 174, 233, 255 };
-    SDL_Color segmentHighlightColor = { 246, 116,   0, 255 };
-    SDL_Color timeIndicatorColor    = { 255, 255, 255, 255 };
-    SDL_Color betweenLineColor      = {  30,  33,  36, 255 };
-    SDL_Color timeLabelColor        = { 180, 180, 180, 255 };
+    SDL_Color segmentOutlineColor   = {  61, 174, 233, 255 }; // Light Blue
+    SDL_Color segmentHighlightColor = { 246, 116,   0, 255 }; // Light Orange
+    SDL_Color timeIndicatorColor    = { 255, 255, 255, 255 }; // White
+    SDL_Color betweenLineColor      = {  30,  33,  36, 255 }; // Dark Gray
+    SDL_Color timeLabelColor        = { 180, 180, 180, 255 }; // Light Gray
 };
